de6604849b8222529ce3ba6868e49fc47e70b9eb
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-09-05 01:24:04 +0100

    dev commit

--- Js/script.js
+++ Js/script.js
@@ -1,67 +1,108 @@
-"use script";
+'use script';
 
 ///////////////////////////////////////
 // Modal window
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const modalForm = document.querySelector(".modal-form");
-const btnCloseModal = document.querySelector(".btn-close-modal");
-const btnOpenModal = document.querySelector(".btn-contact-us");
-const btnSend = document.querySelector(".btn-send");
-const requiredFields = [...document.querySelectorAll(".required")];
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const modalForm = document.querySelector('.modal-form');
+const btnCloseModal = document.querySelector('.btn-close-modal');
+const btnOpenModal = document.querySelector('.btn-contact-us');
+const btnSend = document.querySelector('.btn-send');
+const requiredFields = [...document.querySelectorAll('.required')];
 
 const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
-btnSend.addEventListener("click", function (e) {
-  if (requiredFields.every((field) => field.value !== "")) {
+btnSend.addEventListener('click', function (e) {
+  if (requiredFields.every(field => field.value !== '')) {
     e.preventDefault();
     closeModal();
     modalForm.reset();
   }
 });
 
-btnOpenModal.addEventListener("click", openModal);
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
+btnOpenModal.addEventListener('click', openModal);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
 
 ////////////////////////////////////
 // Smooth Scrolling
-document.querySelector(".mainNavList").addEventListener("click", function (e) {
+document.querySelector('.mainNavList').addEventListener('click', function (e) {
   e.preventDefault();
-  if (e.target.classList.contains("mainNavLink")) {
-    const id = e.target.getAttribute("href");
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  if (e.target.classList.contains('mainNavLink')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
 
-////////////////////////////////////
-// Sticky Nav
-const aboutUs = document.querySelector(".aboutus");
-const nav = document.querySelector(".nav-header");
-const header = document.querySelector(".header");
+document.querySelectorAll('.smooth').forEach(link =>
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = link.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  })
+);
 
-const cords = aboutUs.getBoundingClientRect();
-console.log(cords);
-window.addEventListener("scroll", function () {
-  if (window.scrollY > cords.top) {
-    nav.classList.add("sticky");
-    nav.classList.add("active");
-  } else {
-    nav.classList.remove("sticky");
-    nav.classList.remove("active");
-  }
-});
+////////////////////////////////////////
+// Slider component
+
+const slides = document.querySelectorAll('.slide');
+const btnRight = document.querySelector('.slider-btn--right');
+const btnLeft = document.querySelector('.slider-btn--left');
+
+let counter = 0;
+let currentSlide = 0;
+const maxSlide = slides.length;
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${119 * (i - slide)}%)`)
+  );
+  counter = 0;
+};
+
+goToSlide(0);
+
+const nextSlide = function () {
+  if (currentSlide === maxSlide - 2) currentSlide = 0;
+  else currentSlide++;
+
+  goToSlide(currentSlide);
+};
+
+const previousSlide = function () {
+  if (currentSlide === 0) currentSlide = maxSlide - 2;
+  else currentSlide--;
+
+  goToSlide(currentSlide);
+};
+
+btnRight.addEventListener('click', previousSlide);
+btnLeft.addEventListener('click', nextSlide);
+
+setInterval(() => {
+  counter++;
+}, 1000);
+
+setInterval(() => {
+  if (counter === 5) nextSlide();
+  else counter = 0;
+}, 5000);
+
+////////////////////////////////////////
+// Slider responsiveness
+const page = document.documentElement;
+console.log(page.clientWidth);
